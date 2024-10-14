bea20fe720cfa71cbc2ff0bfbea1b2540067398f
Hoanganhdev111 <demonkate0011@gmail.com>
2024-10-14 22:43:29 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #Downloads
-curl -s -o login.sh -L "https://raw.githubusercontent.com/JohnnyNetsec/github-vm/main/mac/login.sh"
+curl -s -o login.sh -L "https://raw.githubusercontent.com/Hoanganhdev111/luu_tru/refs/heads/main/login.sh"
 #disable spotlight indexing
 sudo mdutil -i off -a
 #Create new account
